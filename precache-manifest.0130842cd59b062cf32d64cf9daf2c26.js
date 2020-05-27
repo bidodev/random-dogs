7d3130e6ec8b86984b59ecb989d7b5e7e5ba39bd
Claudinei Bido <bido.c@live.com>
2020-05-27 15:39:26 +0200

    Updates

---
+++ precache-manifest.0130842cd59b062cf32d64cf9daf2c26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faf5c837bb5350f6da2ae38ad4efc0e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "140a8045416ab10db848",
+    "url": "/static/css/main.6162aef4.chunk.css"
+  },
+  {
+    "revision": "170e11947886e6dbb5b4",
+    "url": "/static/js/2.313c6d6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.313c6d6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "140a8045416ab10db848",
+    "url": "/static/js/main.52b27b1c.chunk.js"
+  },
+  {
+    "revision": "944b5313b947ac15c983",
+    "url": "/static/js/runtime-main.90a70b32.js"
+  }
+]);
